f47d9980350edf3ae884fe6fc43e33a77a55d24f
vladagr-151107 <vladagrushchenko@gmail.com>
2024-11-06 23:06:55 +0200

    hw5 update

---
+++ src/HW5_right/Four.java
@@ -0,0 +1,7 @@
+package HW5_right;
+
+public class Four {
+    public static void main(String[] args){
+
+    }
+}
--- src/HW5_right/Three.java
+++ src/HW5_right/Three.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class Three {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Create a password using letters and digits. At least 8 symbols");
+        System.out.println("Create a password using letters and digits. At least 8 symbols.");
+        String s = scanner.nextLine();
+        if(check(s)){
+            System.out.println("The password is valid");
+        } else{
+            System.out.println("It does not meet requirements.");
+        }
+    }
+    public static boolean check(String str) {
+        for (char ch : str.toCharArray()) {
+            if (!Character.isLetterOrDigit(ch) || str.length() < 8) {
+                return false;
+            }
+        }
+        return true;
     }
 }
